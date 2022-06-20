adf3f4ac4adfc97cf4c62f3094b38de165c052f4
Екатерина Вахонина <102897264+Katyha1992@users.noreply.github.com>
2022-06-20 12:36:55 +0500

    Add files via upload

---
+++ initial_data.py
@@ -0,0 +1,13 @@
+TOKEN = "5498226229:AAElAszUKF9dSOaejF-ibrnAWoTXeobjD5M"
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+}
+
+countcy = 0
+for key in keys.keys():
+    countcy += 1
